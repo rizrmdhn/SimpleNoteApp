9faae46c4f942ffde5f677d993c00d1298785f40
rizrmdhn <rizrmdhn.work@gmail.com>
2023-10-19 11:04:56 +0800

    added tailwind

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+import {SafeAreaView, Text, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <View className="bg-slate-600">
+        <Text className="text-red-500">Hello World</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
